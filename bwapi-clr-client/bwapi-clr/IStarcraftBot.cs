4d722fe247fd79d2ec57503e96a691014be19a21
dpershouse <dpershouse@01bca736-e8ff-11de-9c2a-cb0f5ba72f9d>
2010-10-23 13:53:55 +0000

    Implemented an AIModuleLoader style loader for bots implementing IStarcraftBot. Auto patches references from monobridgeai-interop to bwapi-clr if they are inprocess bots.

---
+++ bwapi-clr-client/bwapi-clr/IStarcraftBot.cs
@@ -0,0 +1,28 @@
+
+using System;
+
+namespace BWAPI
+{
+	/// <summary>
+	/// Base Class for Mono Starcraft Bots
+	/// </summary>
+	public interface IStarcraftBot
+	{
+		void onStart();
+		void onEnd(bool isWinner);
+		void onFrame();
+		void onSendText(string text);
+		void onReceiveText(BWAPI.Player player, string text);
+		void onPlayerLeft(BWAPI.Player player);
+		void onNukeDetect(BWAPI.Position target);
+		void onUnitDiscover(BWAPI.Unit unit);
+		void onUnitEvade(BWAPI.Unit unit);
+		void onUnitShow(BWAPI.Unit unit);
+		void onUnitHide(BWAPI.Unit unit);
+		void onUnitCreate(BWAPI.Unit unit);
+		void onUnitDestroy(BWAPI.Unit unit);
+		void onUnitMorph(BWAPI.Unit unit);
+		void onUnitRenegade(BWAPI.Unit unit);
+		void onSaveGame(string gameName);
+	}
+}
