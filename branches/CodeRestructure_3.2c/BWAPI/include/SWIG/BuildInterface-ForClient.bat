0e30b087325d18db6041be106ffe785cc6aa6f21
dpershouse <dpershouse@01bca736-e8ff-11de-9c2a-cb0f5ba72f9d>
2010-11-06 11:53:49 +0000

    Split swig file to put bwapiclient in separate folders/modules

--- branches/CodeRestructure_3.2c/BWAPI/include/SWIG/BuildInterface-ForClient.bat
+++ branches/CodeRestructure_3.2c/BWAPI/include/SWIG/BuildInterface-ForClient.bat
@@ -9,15 +9,22 @@ SET SWIGPATH=..\..\..\..\swigwin-2.0.1
 
 erase /s /q ..\..\..\bwapi-clr-client\bwapi-clr\swig-classes\*.*
 
-%SWIGPATH%\swig.exe -csharp -c++ -outdir ..\..\..\bwapi-clr-client\bwapi-clr\swig-classes\BWAPI -namespace BWAPI -DINCLUDE_CLIENT -dllimport bwapi-native.dll bwapi-bridge.i
+%SWIGPATH%\swig.exe -csharp -c++ -outdir ..\..\..\bwapi-clr-client\bwapi-clr\swig-classes\BWAPI -namespace SWIG.BWAPI -dllimport bwapi-native.dll bwapi-bridge.i
 
-%SWIGPATH%\swig.exe -csharp -c++ -outdir ..\..\..\bwapi-clr-client\bwapi-clr\swig-classes\BWTA -namespace BWTA -dllimport bwapi-native.dll bwta-bridge.i
+%SWIGPATH%\swig.exe -csharp -c++ -outdir ..\..\..\bwapi-clr-client\bwapi-clr\swig-classes\BWTA -namespace SWIG.BWTA -dllimport bwapi-native.dll bwta-bridge.i
+
+%SWIGPATH%\swig.exe -csharp -c++ -outdir ..\..\..\bwapi-clr-client\bwapi-clr\swig-classes\BWAPIClient -namespace SWIG.BWAPIC -dllimport bwapi-native.dll bwapiclient-bridge.i
+
 
 erase /q ..\..\..\bwapi-clr-client\bwapi-native\bwapi-bridge_wrap.cxx
 erase /q ..\..\..\bwapi-clr-client\bwapi-native\bwta-bridge_wrap.cxx
+erase /q ..\..\..\bwapi-clr-client\bwapi-native\bwapiclient-bridge_wrap.cxx
+
 
 copy bwapi-bridge_wrap.cxx ..\..\..\bwapi-clr-client\bwapi-native\bwapi-bridge_wrap.cxx
 copy bwta-bridge_wrap.cxx ..\..\..\bwapi-clr-client\bwapi-native\bwta-bridge_wrap.cxx
+copy bwapiclient-bridge_wrap.cxx ..\..\..\bwapi-clr-client\bwapi-native\bwapiclient-bridge_wrap.cxx
 
 pause
+goto start
 
