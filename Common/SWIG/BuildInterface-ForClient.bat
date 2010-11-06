a036e682fe0f3bf84e39953b3aef484f66c226a0
dpershouse <dpershouse@01bca736-e8ff-11de-9c2a-cb0f5ba72f9d>
2010-11-06 13:06:37 +0000

    Refactor of swig code to create a library that can produce both embedded and clr dlls with a #define

--- Common/SWIG/BuildInterface-ForClient.bat
+++ Common/SWIG/BuildInterface-ForClient.bat
@@ -5,25 +5,25 @@ REM
 
 
 :start
-SET SWIGPATH=..\..\..\..\swigwin-2.0.1
+SET SWIGPATH=..\..\..\swigwin-2.0.1
 
-erase /s /q ..\..\..\bwapi-clr-client\bwapi-clr\swig-classes\*.*
+erase /s /q ..\..\bwapi-clr-client\bwapi-clr\swig-classes\*.*
 
-%SWIGPATH%\swig.exe -csharp -c++ -outdir ..\..\..\bwapi-clr-client\bwapi-clr\swig-classes\BWAPI -namespace SWIG.BWAPI -dllimport bwapi-native.dll bwapi-bridge.i
+%SWIGPATH%\swig.exe -csharp -c++ -I..\BWAPI\Include -outdir ..\..\bwapi-clr-client\bwapi-clr\swig-classes\BWAPI -namespace SWIG.BWAPI -dllimport \"+importdll+\" bwapi-bridge.i
 
-%SWIGPATH%\swig.exe -csharp -c++ -outdir ..\..\..\bwapi-clr-client\bwapi-clr\swig-classes\BWTA -namespace SWIG.BWTA -dllimport bwapi-native.dll bwta-bridge.i
+%SWIGPATH%\swig.exe -csharp -c++ -I..\BWAPI\Include -outdir ..\..\bwapi-clr-client\bwapi-clr\swig-classes\BWTA -namespace SWIG.BWTA -dllimport \"+importdll+\" bwta-bridge.i
 
-%SWIGPATH%\swig.exe -csharp -c++ -outdir ..\..\..\bwapi-clr-client\bwapi-clr\swig-classes\BWAPIClient -namespace SWIG.BWAPIC -dllimport bwapi-native.dll bwapiclient-bridge.i
-
-
-erase /q ..\..\..\bwapi-clr-client\bwapi-native\bwapi-bridge_wrap.cxx
-erase /q ..\..\..\bwapi-clr-client\bwapi-native\bwta-bridge_wrap.cxx
-erase /q ..\..\..\bwapi-clr-client\bwapi-native\bwapiclient-bridge_wrap.cxx
+%SWIGPATH%\swig.exe -csharp -c++ -I..\BWAPI\Include -outdir ..\..\bwapi-clr-client\bwapi-clr\swig-classes\BWAPIClient -namespace SWIG.BWAPIC -dllimport \"+importdll+\" bwapiclient-bridge.i
 
 
-copy bwapi-bridge_wrap.cxx ..\..\..\bwapi-clr-client\bwapi-native\bwapi-bridge_wrap.cxx
-copy bwta-bridge_wrap.cxx ..\..\..\bwapi-clr-client\bwapi-native\bwta-bridge_wrap.cxx
-copy bwapiclient-bridge_wrap.cxx ..\..\..\bwapi-clr-client\bwapi-native\bwapiclient-bridge_wrap.cxx
+erase /q ..\..\bwapi-clr-client\bwapi-native\bwapi-bridge_wrap.cxx
+erase /q ..\..\bwapi-clr-client\bwapi-native\bwta-bridge_wrap.cxx
+erase /q ..\..\bwapi-clr-client\bwapi-native\bwapiclient-bridge_wrap.cxx
+
+
+copy bwapi-bridge_wrap.cxx ..\..\bwapi-clr-client\bwapi-native\bwapi-bridge_wrap.cxx
+copy bwta-bridge_wrap.cxx ..\..\bwapi-clr-client\bwapi-native\bwta-bridge_wrap.cxx
+copy bwapiclient-bridge_wrap.cxx ..\..\bwapi-clr-client\bwapi-native\bwapiclient-bridge_wrap.cxx
 
 pause
 goto start
